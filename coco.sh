b58469dba50fd4f7395914c98624a1e8d24e58b2
junha1125 <sb020518@naver.com>
2021-04-08 06:23:21 +0000

    Update 2021.04.08 3:25pm

---
+++ coco.sh
@@ -0,0 +1,43 @@
+mkdir coco
+cd coco
+mkdir images
+cd images
+
+wget -c http://images.cocodataset.org/zips/train2017.zip
+wget -c http://images.cocodataset.org/zips/val2017.zip
+wget -c http://images.cocodataset.org/zips/test2017.zip
+wget -c http://images.cocodataset.org/zips/unlabeled2017.zip
+
+unzip train2017.zip
+unzip val2017.zip
+unzip test2017.zip
+unzip unlabeled2017.zip
+
+rm train2017.zip
+rm val2017.zip
+rm test2017.zip
+rm unlabeled2017.zip 
+
+cd ../
+wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget -c http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
+wget -c http://images.cocodataset.org/annotations/image_info_test2017.zip
+wget -c http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
+
+unzip annotations_trainval2017.zip
+unzip stuff_annotations_trainval2017.zip
+unzip image_info_test2017.zip
+unzip image_info_unlabeled2017.zip
+
+rm annotations_trainval2017.zip
+rm stuff_annotations_trainval2017.zip
+rm image_info_test2017.zip
+rm image_info_unlabeled2017.zip
+
+cd /workspace/coco/annotations
+unzip stuff_train2017_pixelmaps.zip
+unzip stuff_val2017_pixelmaps.zip
+
+rm stuff_train2017_pixelmaps.zip
+rm stuff_val2017_pixelmaps.zip
+cd /workspace
